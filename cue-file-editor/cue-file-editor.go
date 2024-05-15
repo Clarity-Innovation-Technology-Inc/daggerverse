1a5d0be77255f621b47f554a884b2292abf493a9
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-05-15 15:24:23 +0300

    adding cue-file-editor (#2)
    * adding cue-file-editor
    
    * Update cue-file-editor.go
    
    * Update cue-file-editor.go
    
    * better setters and fewer optional values
    
    * clean up error handling
    
    * update spelling
    
    * update cue-file-editor api

---
+++ cue-file-editor/cue-file-editor.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"dagger/cue-file-editor/internal/cue"
+	"dagger/cue-file-editor/internal/github"
+)
+
+type CueFileEditor struct {
+	GithubOwner          string
+	GithubRepo           string
+	GithubBranch         string
+	GithubCommitterName  string
+	GithubCommitterEmail string
+	GithubCommitMessage  string
+	GithubRepoPath       string
+	CueFileName          string
+	CuePath              string
+	NewStringValue       string
+}
+
+func New(
+	// the owner of the github org that stores the cue file you want to edit
+	//
+	githubOwner string,
+
+	// the name of the repo that stores the cue file you want to edit
+	//
+	githubRepo string,
+
+	// the branch of the repo that stores the cue file you want to edit
+	//
+	//+optional
+	//+default="main"
+	githubBranch string,
+
+	// the name of the github user that will commit the changes to your cue file
+	//
+	githubCommiterName string,
+
+	// the email of the github user that will commit the changes to your cue file
+	//
+	githubCommiterEmail string,
+
+	// the commit message that will be used when committing the cue file changes
+	//
+	githubCommitMessage string,
+
+	// the subdirectory within the github repo that contains the cue file
+	//
+	githubRepoPath string,
+
+	// the name of the cue config file you want to update
+	//
+	//+optional
+	//+default="values.cue"
+	cueFile string,
+
+	// a cue path (dot seperated) to a specific key within a cue configuration file
+	//
+	cuePath string,
+
+	// a concrete string value you want to use to update your cue file
+	//
+	newStringValue string,
+
+) *CueFileEditor {
+	return &CueFileEditor{
+		GithubOwner:          githubOwner,
+		GithubRepo:           githubRepo,
+		GithubBranch:         githubBranch,
+		GithubCommitterName:  githubCommiterName,
+		GithubCommitterEmail: githubCommiterEmail,
+		GithubCommitMessage:  githubCommitMessage,
+		GithubRepoPath:       githubRepoPath,
+		CueFileName:          cueFile,
+		CuePath:              cuePath,
+		NewStringValue:       newStringValue,
+	}
+}
+
+func (c *CueFileEditor) WithGithubBranch(branch string) *CueFileEditor {
+	c.GithubBranch = branch
+	return c
+}
+func (c *CueFileEditor) WithCueFile(filename string) *CueFileEditor {
+	c.CueFileName = filename
+	return c
+}
+
+func (c *CueFileEditor) Update(token *Secret) {
+	url := fmt.Sprintf("https://github.com/%s/%s.git", c.GithubOwner, c.GithubRepo)
+	dir := dag.Git(url).
+		WithAuthToken(token).
+		Branch(c.GithubBranch).
+		Tree()
+	cueFile := dir.File(fmt.Sprintf("%s/%s", c.GithubRepoPath, c.CueFileName))
+	_, err := cueFile.Export(context.Background(), c.CueFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// decoding the cue values as go data stored in memory
+	values, err := cue.Decode(c.CueFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// finally editing the image tag to the desired tag
+	newCueVal, err := cue.UpdateValues(values, c.CuePath, c.NewStringValue)
+	if err != nil {
+		panic(err)
+	}
+
+	// writing the updated cue file back to disk
+	err = os.WriteFile(c.CueFileName, []byte(fmt.Sprint(newCueVal)), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	tokenString, err := token.Plaintext(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	// now commit this damn thing to github and were good
+	cli := github.New(
+		tokenString,
+		c.GithubOwner,
+		c.GithubRepo,
+		c.GithubBranch,
+		c.GithubRepoPath,
+		[]string{c.CueFileName},
+		c.GithubCommitterName,
+		c.GithubCommitterEmail,
+		c.GithubCommitMessage,
+	)
+
+	ref, statusCode, err := cli.Commit()
+	if err != nil {
+		msg := fmt.Sprintf("commit failed with status code: %d. %v", statusCode, err)
+		panic(msg)
+	}
+
+	if statusCode == http.StatusOK {
+		fmt.Printf("commit succeeded with status code: %d. New Commit SHA: %s\n", statusCode, *ref.Object.SHA)
+	}
+}
---
+++ cue-file-editor/go.mod
@@ -0,0 +1,54 @@
+module dagger/cue-file-editor
+
+go 1.21.7
+
+require (
+	cuelang.org/go v0.8.2
+	github.com/99designs/gqlgen v0.17.44
+	github.com/Khan/genqlient v0.7.0
+	github.com/google/go-github/v61 v61.0.0
+	github.com/kubevela/workflow v0.6.0
+	github.com/vektah/gqlparser/v2 v2.5.11
+	go.opentelemetry.io/otel v1.24.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.24.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.24.0
+	go.opentelemetry.io/otel/sdk v1.24.0
+	go.opentelemetry.io/otel/trace v1.24.0
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
+	golang.org/x/sync v0.7.0
+	google.golang.org/grpc v1.63.2
+)
+
+require (
+	cuelabs.dev/go/oci/ociregistry v0.0.0-20240314152124-224736b49f2e // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
+	github.com/emicklei/proto v1.10.0 // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/protocolbuffers/txtpbfmt v0.0.0-20230328191034-3462fbc510c0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/sosodev/duration v1.2.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240325203815-454cdb8f5daa // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
